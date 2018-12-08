5819ee221a9217fecf753b915e2b060bcbb7ec8b
phansonloc1999 <phansonloc1999@gmail.com>
2018-12-08 14:35:23 +0700

    Add some sound effects

--- src/constants.lua
+++ src/constants.lua
@@ -4,7 +4,17 @@ COLORS = {
     BLUE = { 0, 0, 255 },
     YELLOW = { 255, 255, 0 },
     ORANGE = { 255, 139, 0 },
+    PURPLE = { 128, 0, 128 },
+    BROWN = { 165, 42, 42 },
     WHITE = { 255, 255, 255 }
 }
 
 CURSOR = { WIDTH = 5, HEIGHT = 5 }
+
+gSounds = {
+    ["select"] = love.audio.newSource("assets/select.wav", "static"),
+    ["matched"] = love.audio.newSource("assets/matched.wav", "static"),
+    ["mismatched"] = love.audio.newSource("assets/mismatched.wav", "static")
+}
+
+gSounds["select"]:setVolume(1)
